5b3e677f6d6e18d50f50347e4024dee7019cb44a
Zeng <Frank.Zeng@harman.com>
2022-02-16 15:08:01 +0800

    提交变更

--- entry.js
+++ entry.js
@@ -1,4 +1,5 @@
 
+
  let dashboard
  function onDashboardLoad(payload) {
      console.log("Do something when the dashboard is fully loaded.");
@@ -33,9 +34,119 @@
  }
 
   function getEmbedDashboardUrl() {
-	window.fetch = require('node-fetch');
-    const AWS = require('aws-sdk');
-    generateEmbedUrlForRegisteredUser();
+	 //window.fetch = require('node-fetch');
+     //const AWS = require('aws-sdk');
+     function errorCallback(){
+        console.log(123)
+    }
+    function   getEmbedUrlCallback(){
+        console.log(123)
+    } 
+    var paramsData = {
+ 	    IdentityPoolId: "us-east-1:ff16daae-9dda-4e45-a95c-9fa8c5b939f7"
+    };
+
+// set the Amazon Cognito region
+ AWS.config.region = 'us-east-1';
+// // initialize the Credentials object with our parameters
+ AWS.config.credentials = new AWS.CognitoIdentityCredentials(paramsData);
+ AWS.config.credentials.get(function(err,data) {
+    if ( !err ) {
+         var cognitoSyncClient = new AWS.CognitoSync();
+ cognitoSyncClient.listDatasets({
+     IdentityId: AWS.config.credentials.identityId,
+     IdentityPoolId: "us-east-1:ff16daae-9dda-4e45-a95c-9fa8c5b939f7"
+     // IdentityPoolId: "us-east-1:76b239eb-08cc-4712-ad0c-70eb09823cfa"
+ }, function(err, data) {
+     if ( !err ) {
+        let temdata=AWS.config.credentials;
+        var params = {
+            IdentityPoolId: 'us-east-1:ff16daae-9dda-4e45-a95c-9fa8c5b939f7', /* required */
+            IdentityId: temdata._identityId,
+            TokenDuration: '5000000',
+            Logins:{}
+          };
+          var cognitoidentity = new AWS.CognitoIdentity();
+          //params.Logins['cognitoDeveloperProvidedName'] = 'Dhamodharan.Subramanian@harman.com'; 
+          cognitoidentity.getOpenIdTokenForDeveloperIdentity(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(data);           // successful response
+          });
+
+      
+   }
+     else{      
+         generateEmbedUrlForRegisteredUser(722436621188,'4ffc8fc5-2261-4d00-8b85-024d0399aa5c',data,'arn:aws:iam::722436621188:user/quicksigth_testuser','arn:aws:iam::722436621188:role/quicksightembed','12121',getEmbedUrlCallback,errorCallback);  
+     }
+ });
+    //generateEmbedUrlForRegisteredUser(722436621188,'4ffc8fc5-2261-4d00-8b85-024d0399aa5c',temdata.sessionToken,'arn:aws:iam::722436621188:user/quicksigth_testuser','arn:aws:iam::722436621188:role/Cognito_jblwebportalAuth_Role',temdata.params.RoleSessionName,getEmbedUrlCallback,errorCallback);  
+   }
+ })
+
+//  var cognitoSyncClient = new AWS.CognitoSync();
+//  cognitoSyncClient.listDatasets({
+//      IdentityId: AWS.config.credentials.identityId,
+//      IdentityPoolId: "us-east-1:ff16daae-9dda-4e45-a95c-9fa8c5b939f7"
+//      // IdentityPoolId: "us-east-1:76b239eb-08cc-4712-ad0c-70eb09823cfa"
+//  }, function(err, data) {
+//      if ( !err ) {
+//          debugger;
+//          var cognitoidentity = new AWS.CognitoIdentity();
+//          var params = {
+//              AllowUnauthenticatedIdentities: true, /* required */
+//              IdentityPoolName: 'jblwebportal', /* required */
+//              OpenIdConnectProviderARNs:['arn:aws:iam::722436621188:oidc-provider/cognito-idp.us-east-1.amazonaws.com/us-east-1_HiWhDwBfC']
+//            };
+//            cognitoidentity.createIdentityPool(params, function(err, data) {
+//              if (err) console.log(err, err.stack); // an error occurred
+//              else     console.log(data);           // successful response
+//            });
+
+      
+//    }
+//      else{      
+//          generateEmbedUrlForRegisteredUser(722436621188,'4ffc8fc5-2261-4d00-8b85-024d0399aa5c',data,'arn:aws:iam::722436621188:user/quicksigth_testuser','arn:aws:iam::722436621188:role/quicksightembed','12121',getEmbedUrlCallback,errorCallback);  
+//      }
+//  });
+// AWS.config.credentials.get(function(err,data) {
+// 	if (err) {
+// 		console.log("Error: "+err);
+// 		return;
+// 	}
+// 	console.log("Cognito Identity Id: " + AWS.config.credentials.identityId);
+
+// // 	// Other service clients will automatically use the Cognito Credentials provider
+// // 	// configured in the JavaScript SDK.
+// 	var cognitoSyncClient = new AWS.CognitoSync();
+// 	cognitoSyncClient.listDatasets({
+// 		IdentityId: AWS.config.credentials.identityId,
+//         IdentityPoolId: "us-east-1:ff16daae-9dda-4e45-a95c-9fa8c5b939f7"
+// 		// IdentityPoolId: "us-east-1:76b239eb-08cc-4712-ad0c-70eb09823cfa"
+// 	}, function(err, data) {
+// 		if ( !err ) {
+//             var cognitoidentity = new AWS.CognitoIdentity();
+//             var params = {
+//                 AllowUnauthenticatedIdentities: true, /* required */
+//                 IdentityPoolName: 'jblwebportal', /* required */
+//                 OpenIdConnectProviderARNs:['arn:aws:iam::722436621188:oidc-provider/cognito-idp.us-east-1.amazonaws.com/us-east-1_HiWhDwBfC']
+//               };
+//               cognitoidentity.createIdentityPool(params, function(err, data) {
+//                 if (err) console.log(err, err.stack); // an error occurred
+//                 else     console.log(data);           // successful response
+//               });
+
+         
+//       }
+//         else{      
+//             generateEmbedUrlForRegisteredUser(722436621188,'4ffc8fc5-2261-4d00-8b85-024d0399aa5c',data,'arn:aws:iam::722436621188:user/quicksigth_testuser','arn:aws:iam::722436621188:role/quicksightembed','12121',getEmbedUrlCallback,errorCallback);  
+//         }
+// 	});
+// })
+
+//     let temdata=AWS.config.credentials;
+// generateEmbedUrlForRegisteredUser(722436621188,'4ffc8fc5-2261-4d00-8b85-024d0399aa5c',temdata.sessionToken,'arn:aws:iam::722436621188:user/quicksigth_testuser','arn:aws:iam::722436621188:role/Cognito_jblwebportalAuth_Role',temdata.params.RoleSessionName,getEmbedUrlCallback,errorCallback); 
+// }); 
+        // successful respons
     function generateEmbedUrlForRegisteredUser(
         accountId,
         dashboardId,
@@ -101,6 +212,7 @@
         });
     }
   }
+  
 
 
 
